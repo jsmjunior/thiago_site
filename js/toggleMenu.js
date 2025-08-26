9ed21fd592e1541d2cfd43ff93d4ac9e39e0dca1
José de Souza <jose441232@gmail.com>
2025-08-26 07:34:01 +0100

    changes on responsivit and add the button to shcedule

--- js/toggleMenu.js
+++
@@ -1,6 +0,0 @@
-// Toggle the visibility of the menu
-const menu = document.getElementById("menu-button");
-
-function toggleMenu() {
-    menu.classList.toggle("hidden");
-}
---
+++ js/utils.js
@@ -0,0 +1,4 @@
+
+function goToSchedule() {
+    return window.open("https://www.trgclub.com/terapeuta/thiago-oliveira-2025-06-16-17-24-15-0300", "_blank");
+}
